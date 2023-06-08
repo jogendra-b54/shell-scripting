0e7d2d2990baea004a9cc3477da73e9490921875
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-08 22:00:02 +0530

    Auto push

--- variable.sh
+++ variable.sh
@@ -9,6 +9,7 @@ a=10
 b=20
 c=30        
 d=40
+e=70
 
 echo $a
 
@@ -16,3 +17,4 @@ echo -e "Value of the variable a is \e[32m $a \e[0m"
 echo -e "Value of the variavle b is \e[32m $b \e[0m"
 echo -e "Value of the variable c is \e[32m $c \e[0m"
 echo -e "Value of the variable d is \e[32m $d \e[0m"
+echo -e "Value of the variable e is \e[32m ${e} \e[0m"
