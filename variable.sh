95e3a941cb3a3899e018adb7a040229286dcb529
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-08 21:56:36 +0530

    Auto push

---
+++ variable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+
+# What is a variable ? Variable is ised to store and pass values    
+
+
+# This is how we can declare variables in Bash 
+a=10 
+b=20
+c=30        
+
+
+echo $a
+
+echo -e "Value of the variable a is \e[32m $a \e[0m"
+echo -e "Value of the variavle b is \e[32m $b \e[0m"
+echo -e "Value of the variable c is \e[32m $c \e[0m"
+echo -e "Value of the variable d is \e[32m $d \e[0m"
