e480e5eba45124d74b0ba6e344f8675c6fbc89ed
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-08 21:07:10 +0530

    Auto push

--- color.sh
+++ color.sh
@@ -2,4 +2,9 @@
 
 # Each and every color in terminal will have a color coding
 
+echo -e "\e[31m I am printing Red color \e[0m"
 echo -e "\e[32m I am printing Green color \e[0m"
+echo -e "\e[33m I am printing Yellow color \e[0m"
+echo -e "\e[34m I am printing Blue color \e[0m"
+echo -e "\e[35m I am printing Magenta color \e[0m"
+echo -e "\e[36m I am printing Cyan color \e[0m"
