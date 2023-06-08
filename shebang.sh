971e6407868e516e245b71a9f8e14a1f6f87f486
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-08 20:58:12 +0530

    Auto push

--- shebang.sh
+++ shebang.sh
@@ -8,3 +8,11 @@ echo  Welcome to Batch54 Cloud DevOps Training
 
 echo Line1
 echo Line1
+
+# in bash , we have escape sequence characters , using that we can add some ppower to the echo
+
+# \n : new line
+# \t : tab space
+
+echo -e "Line1\nLine2"
+echo -e "Line1\n\tLine2"
