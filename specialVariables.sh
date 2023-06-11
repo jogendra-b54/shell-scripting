6fae2a4dc0c673a181d84c01a39d9544d5e4de43
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-11 19:35:48 +0530

    Auto push

--- specialVariables.sh
+++ specialVariables.sh
@@ -5,7 +5,7 @@
 # Here are the few special variables :  $0 to $9 , $? $# $* $@
 
 
-echo $0   #==> executed the script name your are executing
+#echo $0   #==> executed the script name your are executing
 echo "Name of the script executed  is $0"
 echo "Name of the student is $1"
 echo "Name of the Training batch is $2"
