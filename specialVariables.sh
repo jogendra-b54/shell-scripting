40b6da4b6b92c114dad246323b9232f7867be513
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-11 19:46:59 +0530

    Auto push

--- specialVariables.sh
+++ specialVariables.sh
@@ -6,12 +6,15 @@
 
 
 #echo $0   #==> executed the script name your are executing
-echo "Name of the script executed  is $0"
-echo "Name of the student is $1"
-echo "Name of the Training batch is $2"
-echo "Current topics is  $3"
-#echo $1 
-#echo $?
-#echo $#
-#echo $*
-#echo $@
+echo "Name of the script executed  is : $0"
+echo "Name of the student is : $1"
+echo "Name of the Training batch is : $2"
+echo "Current topics is  : $3"
+echo "\n"
+echo "\nDisplaying all the special varriables "
+
+echo "the used variables are :$*"
+echo "the exist code of this script is: $? "
+echo "the total number of arguments is: $# "
+echo "the PID of the script is : $$"
+echo "it will display all the arguemnts  $@ "
