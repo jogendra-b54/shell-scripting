56108b20d88583661d243b44a85f18fd67144392
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-11 21:26:09 +0530

    Auto push

---
+++ 08-function.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#functon is to capture the repeatative block
+
+sample()
+{
+    echo "I am a sample function with sample name "
+    echo "I am executing the sample function"
+    echo "sample function is completed"
+
+}
+
+#this is how you can call a function
