263166367fbc31569c852f69338072aa99f2f2f7
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-12 17:52:10 +0530

    Auto push

---
+++ case.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ACTION=$1
+
+case $ACTION in 
+    start)
+        echo "strating rabbitMQ services"
+        ;;
+        stop)
+        echo "stopping rabbitMQ services"
+        ;;
+        restart) 
+        echo "Restarting rabbitMQ services"
+        ;;
+        *)
+        echo "Possible values are start or stop or restart only"
+        ;;  
+
+ease 
