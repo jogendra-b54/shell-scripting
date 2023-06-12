7bab93dc123e2f8680a1438f624ec01f191449ec
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-12 17:10:57 +0530

    Auto push

--- 10-quotes.sh
+++ 10-quotes.sh
@@ -3,4 +3,6 @@
 # In bash, output of your command will be complete changes based on the quotes you use : 
 
 a=53
-echo "Value of a is ${a}
+echo "Value of a is ${a}"
+echo Value of a is ${a}
+echo 'Value of a is ${a}'
