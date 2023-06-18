2ec5866dc647ca49bc09483b48d62d21a348cb06
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-18 20:04:20 +0530

    Auto push

--- Pass_function_as_Parameter.sh
+++ Pass_function_as_Parameter.sh
@@ -11,7 +11,19 @@ function Around()
     echo "before"
    # eval $1
    var=$($1) # instead of using 'eval' we can use this way
+   # echo "after"
     echo "after $var"
 }
 
 Around x
+
+#--------------------------------------------------------------
+
+function myfunc()
+{
+    local  myresult='some value'
+    echo "$myresult"
+}
+
+result=$(myfunc)   # or result=`myfunc`
+echo $result
