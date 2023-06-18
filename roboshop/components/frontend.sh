5bee8187a7acb463ef14002f9db7eb0698e8efa9
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-18 21:00:40 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "I am frontend"
+echo "Installing Nginx :"
+
+yum install nginx -y
+
+# The frontend is the service in RobotShop to serve the web content over Nginx.
+
+# Install Nginx.
+
+# ```
+# # yum install nginx -y
+# # systemctl enable nginx
+# # systemctl start nginx
+
+# ```
+
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
