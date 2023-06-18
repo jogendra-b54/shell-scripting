c184b99f8630d6d40bfa9ddb628529803012e40a
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-18 23:05:44 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+ID=$(id -u)
+if [$ID -ne 0]; then
+    echo -e "\e[31m This script is executed to be run by a root user or with sudo previlage"
+    exit 1
+fi
+
 echo "Installing Nginx :"
 
 yum install nginx -y
 
+# if the script is executed as root user or sudo user . then it has to proceed
+# if not that mean when script runs as normal user , it should display a nice  message
 # The frontend is the service in RobotShop to serve the web content over Nginx.
 
 # Install Nginx.
