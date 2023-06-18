79df8e55ec4e803c87a6ab204e8ea1721b7fdd5e
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-18 17:06:49 +0530

    Auto push

--- if-else-condition.sh
+++ if-else-condition.sh
@@ -2,7 +2,7 @@
 
 ACTION=$1
 
-if[ "$ACTION" == "start" ]; then
+if[ "$ACTION" == "start" ] ; then
 echo -e "\e[32 starting RabbitMQ server \e[0m"
 else
 echo -e "\e[31 Available Option is start only \e[0m"
