f7200ede4d1a777715d88e20e301a6b86c1a86ef
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-18 17:45:03 +0530

    Auto push

--- if-else-condition.sh
+++ if-else-condition.sh
@@ -2,8 +2,20 @@
 
 ACTION=$1
 
-if [ "$ACTION" == "start" ] ; then
-echo -e "\e[32m starting RabbitMQ server \e[0m"
-else
-echo -e "\e[31m Available Option is start only \e[0m"
-fi 
+if [ "$ACTION" == "start" ]; then
+    echo -e "\e[32m starting RabbitMQ server \e[0m"
+    exit 0
+elif [ "$ACTION" == "restart" ]; then
+    echo -e "\e[33m Restarting RabbitMQ server \e[0m"
+    exit 1
+elif [ "$ACTION" == "stop" ]; then
+    echo -e "\e[31m Stopping the RabbitMQ server \e[0m"
+    exit 2
+elif [ "$ACTION" == "enable" ]; then
+    echo -e "\e[36m Enabling  the RabbitMQ server \e[0m"
+    exit 3
+else 
+    echo -e "\e[34m Available options are start---stop----restart-----enable only \e[0m"
+    exit 4
+
+fi
