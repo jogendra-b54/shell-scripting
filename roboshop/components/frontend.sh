359549335c9c5067ad34d6a19b84639d39604303
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-18 23:14:48 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+COMPONENT=frontend
+
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
     echo -e "\e[31m This script is executed to be run by a root user or with sudo previlage \e[0m"
@@ -8,7 +10,7 @@ fi
 
 echo "Installing Nginx :"
 
-yum install nginx -y
+yum install nginx -y &>> "/tmp/${COMPONENT}.log"
 
 # if the script is executed as root user or sudo user . then it has to proceed
 # if not that mean when script runs as normal user , it should display a nice  message
