fcb2f77d2ddd209630aad52ea6808418f7a48ff6
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-19 10:55:52 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -34,5 +34,10 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans
 stat $?
 
 echo -n " Installing  $COMPONENT  : "
-yum install -y mongodb-org  &>>$LOGFILE
+yum install -y $COMPONENT-org  &>>$LOGFILE
+stat $?
+
+echo -n "Starting $COMPONENT : "
+systemctl enable mongod &>>$LOGFILE
+systemctl start mongod &>>$LOGFILE
 stat $?
