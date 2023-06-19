122456472c9fd279af255b13d54632666a5142fa
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-19 13:15:03 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -32,7 +32,7 @@ echo -n "Configuring the $COMPONENT repo : "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
 
-echo -n " Installing  $COMPONENT  : "
+echo -n "Installing  $COMPONENT  : "
 yum install -y $COMPONENT-org &>>$LOGFILE
 stat $?
 
@@ -55,15 +55,17 @@ stat $?
 # mongo < catalogue.js
 # mongo < users.js
 
-echo -n " Downloading the $COMPONENT schema : "
+echo -n "Downloading the $COMPONENT schema : "
 curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
 stat $?
 
 echo -n "Extracting the $COMPONENT Schema : "
 cd /tmp
 unzip -o mongodb.zip &>>$LOGFILE
+stat $?
 
-echo -n " Injecting the $COMPONENT schema : "
+
+echo -n "Injecting the $COMPONENT schema : "
 cd $COMPONENT-main
 mongo <catalogue.js &>>$LOGFILE
 mongo <users.js &>>$LOGFILE
