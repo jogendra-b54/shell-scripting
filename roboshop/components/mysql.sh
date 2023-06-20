8e4a0c0b54717f5b69b960c108bcec7e828ba6f0
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-20 16:31:21 +0530

    Auto push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -21,7 +21,6 @@ echo -n "Fetching  default root password : "
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 stat $?
 
-
 #mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}
 #ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
 #I want this to be executed only if the default password reset was not done .
@@ -30,13 +29,17 @@ if [ $? -ne 0 ]; then
     echo -n "Performing password reset of MySQL Root user : "
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD} &>>$LOGFILE
     stat $?
+else
+    echo -n "Already the mysql root password has been changed : "
+    stat $?
 fi
-
-
 
 echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password &>>$LOGFILE
 if [ $? -eq 0 ]; then
     echo -n "Uninstalling the validate_password  plugin : "
-    echo "UNINSTALL PLUGIN validate_password;" | mysql -uroot -pRoboShop@1  &>>$LOGFILE
+    echo "UNINSTALL PLUGIN validate_password;" | mysql -uroot -pRoboShop@1 &>>$LOGFILE
+    stat $?
+    else
+    echo -n "Already the mysql validate_password plugins has been Uninstalled  : "
     stat $?
 fi
