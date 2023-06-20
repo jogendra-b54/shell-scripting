382606fe736316fbdfe427c9c7caac37e89e7919
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-20 16:19:38 +0530

    Auto push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -17,7 +17,7 @@ systemctl enable mysqld &>>$LOGFILE
 systemctl start mysqld &>>$LOGFILE
 stat $?
 
-echo -n "Changing  default root password : "
+echo -n "Fetching  default root password : "
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 stat $?
 
