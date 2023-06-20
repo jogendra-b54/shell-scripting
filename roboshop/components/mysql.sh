1424539bae8b712a483e8c03339966fc9db68332
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-20 16:16:32 +0530

    Auto push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -4,32 +4,30 @@ COMPONENT="mysql"
 
 source components/common.sh
 
-
 echo -n "Configuring the $COMPONENT Repo : "
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
 stat $?
 
 echo -n "Installing the $COMPONENT :"
-yum install mysql-community-server -y   &>> $LOGFILE
+yum install mysql-community-server -y &>>$LOGFILE
 stat $?
 
 echo -n "Starting the $COMPONENT : "
-systemctl enable mysqld  &>> $LOGFILE
-systemctl start mysqld   &>> $LOGFILE
+systemctl enable mysqld &>>$LOGFILE
+systemctl start mysqld &>>$LOGFILE
 stat $?
 
 echo -n "Changing  default root password : "
-DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk  '{print $NF}' )
+DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 stat $?
 
-echo -n "Performing password reset of MySQL Root user : "
+
 #mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}
 #ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}  &>> $LOGFILE
-stat $?
-
-
-
-
-
-
+#I want this to be executed only if the default password reset was not done .
+echo "show databases;" | mysql -uroot -RoboShop@1 &>>$LOGFILE
+if [ $? -ne 0 ]; then
+    echo -n "Performing password reset of MySQL Root user : "
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD} &>>$LOGFILE
+    stat $?
+fi
