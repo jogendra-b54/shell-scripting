5a44bb64d91247792eadbb08ea3f38250490bb3e
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-22 17:06:42 +0530

    Auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -4,6 +4,8 @@
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId')
 
+COMPONENT=$1
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$( aws ec2 describe-security-groups --filters "Name=group-name,Values=b54-allow-all" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 #echo -e "AMI ID used to launch the EC2 is \e[32m $AMI_ID \e[0m \033[5mYOUR_STRING\033[0m" ) for bold Italic blink
@@ -22,4 +24,4 @@ echo -e " ***********Lanuching the Server ********** "
  #aws ec2 run-instances   --image-id ${AMI_ID}   --instance-type t3.micro | jq .
 
  #--tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]'
- aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=payment}]' | jq .
+ aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
