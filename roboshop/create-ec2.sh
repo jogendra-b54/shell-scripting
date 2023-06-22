3dc92db8d04cf1d4c5cb1f2ac025187297dad7f2
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-22 18:49:40 +0530

    Auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -5,15 +5,15 @@
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId')
 
 COMPONENT=$1
-
-if [ -z "$1" ] ; then
+HOSTEDZONEID=Z08104102TDJJFARM8HPK
+if [ -z "$1" ]; then
     echo -e "\e[31m COMPONENT NAME IS REQUIRED TO SUPPLY \e[0m "
     echo -e "\e[35m Ex Usage : \n \t  \t bash create-ec2 componentName \e[0m "
     exit 1
 fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
-SG_ID=$( aws ec2 describe-security-groups --filters "Name=group-name,Values=b54-allow-all" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b54-allow-all" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 #echo -e "AMI ID used to launch the EC2 is \e[32m $AMI_ID \e[0m \033[5mYOUR_STRING\033[0m" ) for bold Italic blink
 
 echo -e "AMI ID used to launch the EC2 is \e[32m $AMI_ID \e[0m"
@@ -25,18 +25,22 @@ echo -e "Security Group  ID used to launch the EC2 is \e[32m $SG_ID \e[0m"
 #aws ec2 describe-security-groups --filters "Name=group-name,Values=b54-allow-all"
 # aws ec2 describe-security-groups --filters "Name=group-name,Values=b54-allow-all" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g'
 
-
 echo -e " \e[36m***********Lanuching the Server ********** \e[0m"
- #aws ec2 run-instances   --image-id ${AMI_ID}   --instance-type t3.micro | jq .
+#aws ec2 run-instances   --image-id ${AMI_ID}   --instance-type t3.micro | jq .
 
- #--tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]'
- #aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
+#--tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]'
+#aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
 
- #aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
+#aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
 
- #aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids $SG_ID \
-   # --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
-IPADDRESS=$(aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids $SG_ID \
+#aws ec2 run-instances  --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids $SG_ID \
+# --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
+IPADDRESS=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-    echo -e "private  IP  Address of $COMPONENT  is \e[35m $IPADDRESS \e[0m"
+echo -e " \e[36m***********Lanuching $COMPONENT server is completed ********** \e[0m"
+echo -e "private  IP  Address of $COMPONENT  is \e[35m $IPADDRESS \e[0m"
+
+echo -n "\e[36m *****Creating DNS record for the $COMPONENT  : ******** \e[0m "
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}" roboshop/route53.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file://route53.json
