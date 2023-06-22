8263ade25ae625cba82dcb14dede48d78c940d5a
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-22 18:57:57 +0530

    Auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -43,4 +43,5 @@ echo -e "private  IP  Address of $COMPONENT  is \e[35m $IPADDRESS \e[0m"
 
 echo -e "\e[36m *****Creating DNS record for the $COMPONENT  : ******** \e[0m "
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/record.json
-aws route53 change-resource-record-sets  --hosted-zone-id $HOSTEDZONEID  --change-batch file://tmp/route53.json
+aws route53 change-resource-record-sets  --hosted-zone-id $HOSTEDZONEID  --change-batch file:///tmp/record.json
+                                                                                        
