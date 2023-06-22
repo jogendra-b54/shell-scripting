402a18ab906d7ddae4491e31959a8038cb97a9a8
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-22 17:34:33 +0530

    Auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -6,6 +6,12 @@
 
 COMPONENT=$1
 
+if [ -z "$1" ] then;
+    echo -e "\e[31m COMPONENT NAME IS REQUIRED TO SUPPLY \e[0m "
+    echo -e "\e[35m Ex Usage : bash create-ec2 componentName \e[0m "
+    exit 1
+fi
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$( aws ec2 describe-security-groups --filters "Name=group-name,Values=b54-allow-all" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 #echo -e "AMI ID used to launch the EC2 is \e[32m $AMI_ID \e[0m \033[5mYOUR_STRING\033[0m" ) for bold Italic blink
