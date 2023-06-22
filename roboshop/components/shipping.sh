7c1a9c2777399730ff3df8808612af558829ef22
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-22 14:13:57 +0530

    Auto push

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -6,3 +6,7 @@ source components/common.sh
 
 JAVA                           # Calling NodeJS Function
 
+aws ec2 run-instances \
+    --image-id ami-0abcdef1234567890 \
+    --instance-type t2.micro \
+    --key-name MyKeyPair
