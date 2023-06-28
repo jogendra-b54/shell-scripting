ecd5a732a53f6555cc2d29b7db48b213c708a402
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-28 22:23:05 +0530

    Auto push

--- roboshop/xyz.sh
+++ roboshop/xyz.sh
@@ -3,8 +3,9 @@
 # AMI_ID="ami-0c1d144c8fdd8d690"
 
 COMPONENT=$1
+HOSTEDZONEID="Z08104102TDJJFARM8HPK"
 ENV=$2
-HOSTEDZONEID="Z08104102TDJJFARM8HPK""
+#HOSTEDZONEID="Z08104102TDJJFARM8HPK""
 
 if [ -z "$1" ] || [ -z "$2" ] ; then
     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m"
@@ -24,13 +25,13 @@ echo -e "\e[36m *** Launching the server **** \e[0m"
 
 IPADDRESS=$(aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t3.micro   --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo -e "\e[36m *** Launching the $COMPONENT server is completed **** \e[0m"
+#echo -e "\e[36m *** Launching the $COMPONENT server is completed **** \e[0m"
 
 echo -e "private IP Address of $COMPONENT is \e[35m $IPADDRESS \e[0m"
 
-echo -e "\e[36m ***** Creating DNS record for the $COMPONENT : ****** \e[0m "
+#echo -e "\e[36m ***** Creating DNS record for the $COMPONENT : ****** \e[0m "
 
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/"  roboshop/route53.json > /tmp/record.json
+#sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/"  roboshop/route53.json > /tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp.record.json
 
 
