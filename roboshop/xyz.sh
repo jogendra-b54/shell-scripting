854805ec43208029a15d6237fbe1e4f17cab81f9
jogendra-b54 <jpatro.kumar@gmail.com>
2023-06-29 00:38:29 +0530

    Auto push

--- roboshop/xyz.sh
+++ roboshop/xyz.sh
@@ -7,7 +7,7 @@ HOSTEDZONEID="Z08104102TDJJFARM8HPK"
 ENV=$2
 #HOSTEDZONEID="Z08104102TDJJFARM8HPK""
 
-if [ -z "$1" ]; then
+if [ -z "$1" ] || [ -z "$2" ] ; then
     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m"
     echo -e "\e[35m Ex Usage : \n \t \t bash create-ec2 componentName envName \e[0m "
     exit 1
